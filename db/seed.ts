95e60c24f240c06ec9fdfad52112e62773750e92
rjaybernardo <raphaeljaybernardo@gmail.com>
2026-09-05 15:28:01 +0800

    14 Seed Sample Data

---
+++ db/seed.ts
@@ -0,0 +1,25 @@
+import { prisma } from "@/lib/prisma";
+
+import sampleData from "./sample-data";
+
+async function main() {
+  console.log("🌱 Seeding database...");
+
+  await prisma.product.deleteMany();
+
+  await prisma.product.createMany({
+    data: sampleData.products,
+  });
+
+  console.log("✅ Database seeded successfully.");
+}
+
+main()
+  .catch((error) => {
+    console.error("❌ Error while seeding the database:");
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
