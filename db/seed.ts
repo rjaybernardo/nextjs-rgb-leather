8c7c50bec222fb107f8c10ccc4629a3621a6f21b
rjaybernardo <raphaeljaybernardo@gmail.com>
2026-09-05 22:48:21 +0800

    21 seed user data

--- db/seed.ts
+++ db/seed.ts
@@ -6,9 +6,17 @@ async function main() {
   console.log("🌱 Seeding database...");
 
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   await prisma.product.createMany({
     data: sampleData.products,
+  });
+
+  await prisma.user.createMany({
+    data: sampleData.users,
   });
 
   console.log("✅ Database seeded successfully.");
