aadbc3bdf1b03a6a4aefc48e6ddf8963a4d191b4
rjaybernardo <raphaeljaybernardo@gmail.com>
2026-09-06 00:41:42 +0800

    27 Hook Up Sign In Form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
+import { auth } from "@/auth";
 import {
   Card,
   CardContent,
@@ -9,14 +11,21 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import CredentialsSignInForm from "./credentials-signin-form";
 import { APP_NAME } from "@/lib/constants";
+
+import CredentialsSignInForm from "./credentials-signin-form";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const session = await auth();
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="mx-auto w-full max-w-md">
       <Card>
