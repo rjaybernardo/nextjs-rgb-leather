de4e96a9d07bab08febf894fab6cd6458ed6a91f
rjaybernardo <raphaeljaybernardo@gmail.com>
2026-09-07 10:56:46 +0800

    36 Set Session Cart ID in Cookie

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "@prisma/adapter-pg",
+    "@prisma/client",
+    "pg",
+    "prisma",
+  ],
 };
 
 export default nextConfig;
--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,33 @@
 import { auth } from "@/auth";
+import { NextResponse } from "next/server";
 
-export const proxy = auth;
+export const proxy = auth((request) => {
+  const sessionCartId = request.cookies.get("sessionCartId")?.value;
+
+  if (sessionCartId) {
+    return NextResponse.next();
+  }
+
+  const response = NextResponse.next();
+
+  response.cookies.set("sessionCartId", crypto.randomUUID(), {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+  });
+
+  return response;
+});
 
 export const config = {
-  matcher: ["/account/:path*", "/admin/:path*"],
+  matcher: [
+    /*
+     * Run Proxy on application pages, but skip:
+     * - API routes
+     * - Next.js internals
+     * - static files
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)",
+  ],
 };
