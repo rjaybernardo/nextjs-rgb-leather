f9a66e1f2ac234ed25aaf7463f22c043ba0ebbaa
rjaybernardo <raphaeljaybernardo@gmail.com>
2026-09-08 19:14:11 +0800

    57 Load Products From Database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,6 @@
 import "server-only";
 
-import { PrismaNeon } from "@prisma/adapter-neon";
-
+import { PrismaNeonHttp } from "@prisma/adapter-neon";
 import { PrismaClient } from "@/lib/generated/prisma/client";
 
 const connectionString = process.env.DATABASE_URL;
@@ -10,9 +9,7 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not set");
 }
 
-const adapter = new PrismaNeon({
-  connectionString,
-});
+const adapter = new PrismaNeonHttp(connectionString, {});
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
