33adb3bb2d78804154aa9f93fbfcf7399174fb54
rjaybernardo <raphaeljaybernardo@gmail.com>
2026-09-09 11:02:05 +0800

    68 Update User Profile Action

--- lib/validators.ts
+++ lib/validators.ts
@@ -197,3 +197,8 @@ export const insertOrderItemSchema = z.object({
       error: "Quantity must be greater than zero",
     }),
 });
+
+export const updateProfileSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  email: z.string().min(3, "Email must be at least 3 characters"),
+});
